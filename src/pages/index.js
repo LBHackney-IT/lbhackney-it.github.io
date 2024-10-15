9a68397e927771674ceeb4fc65a9b31240e7bed6
Ryan Brooks <ryan@slatehorse.com>
2024-10-15 12:00:49 +0100

    🎨 Use Hackney colour scheme

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ function HomepageHeader() {
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
+        <img src="img/logoHackney.png" alt="Hackney logo"></img>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
