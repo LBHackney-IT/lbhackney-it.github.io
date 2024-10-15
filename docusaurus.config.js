97b40e59432572f52480ead9f831285bb39aeb1d
Ryan Brooks <ryan@slatehorse.com>
2024-10-15 16:07:10 +0100

    GitHub Action to deploy to GitHub Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'LBHackney-IT', // Usually your GitHub org/user name.
   projectName: 'lbhackney-it.github.io', // Usually your repo name.
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
